4357287b47d4b3aaf1e64d5fb0a25ea6b6d05518
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-07-18 00:57:48 +0500

    implemented verify token function, update and delete user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,51 @@
-export const test = (req, res) => {
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      const { password, ...others } = updatedUser._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You can update only your account!"));
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "User has been deleted!" });
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You can delete only your account!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  res.json("Success");
+};
+export const subscribe = async (req, res, next) => {
+  res.json("Success");
+};
+export const unsubscribe = async (req, res, next) => {
+  res.json("Success");
+};
+export const like = async (req, res, next) => {
+  res.json("Success");
+};
+export const dislike = async (req, res, next) => {
   res.json("Success");
 };
