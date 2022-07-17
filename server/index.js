94b5def3dfbd564104044b9248c7d268c532238d
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-07-17 23:37:06 +0500

    created routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
 
 const app = express();
 dotenv.config();
@@ -10,6 +14,11 @@ const connect = () => {
     .connect(process.env.MONGO_DB_ACCESS, () => console.log("DB Connected!"))
     .catch(() => console.error("Something went wrong with DB"));
 };
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(8800, () => {
   connect();
