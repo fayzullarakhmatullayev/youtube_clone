65dee72d6ca82d33da297360cd3a52aee63e7ae2
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-07-21 00:25:18 +0500

    created react app

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import { createError } from "../error.js";
 import User from "../models/User.js";
+import Video from "../models/Video.js";
 
 export const updateUser = async (req, res, next) => {
   if (req.params.id === req.user.id) {
@@ -44,7 +45,7 @@ export const getUser = async (req, res, next) => {
 };
 export const subscribe = async (req, res, next) => {
   try {
-    await User.findById(req.user.id, {
+    await User.findByIdAndUpdate(req.user.id, {
       $push: { subscribedUsers: req.params.id },
     });
     await User.findByIdAndUpdate(req.params.id, {
@@ -58,7 +59,7 @@ export const subscribe = async (req, res, next) => {
 };
 export const unsubscribe = async (req, res, next) => {
   try {
-    await User.findById(req.user.id, {
+    await User.findByIdAndUpdate(req.user.id, {
       pull: { subscribedUsers: req.params.id },
     });
     await User.findByIdAndUpdate(req.params.id, {
@@ -71,13 +72,27 @@ export const unsubscribe = async (req, res, next) => {
   }
 };
 export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+    res.status(200).json({ message: "The video has been liked!" });
   } catch (err) {
     next(err);
   }
 };
 export const dislike = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id },
+    });
+    res.status(200).json({ message: "The video has been disliked!" });
   } catch (err) {
     next(err);
   }
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,39 @@
 import express from "express";
-import {} from "../controllers/video.js";
+import {
+  addVideo,
+  updateVideo,
+  deleteVideo,
+  getVideo,
+  addView,
+  random,
+  trend,
+  sub,
+  getByTag,
+  search,
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+// Create a video
+router.post("/", verifyToken, addVideo);
+// Update a video
+router.put("/:id", verifyToken, updateVideo);
+// Delete a video
+router.delete("/:id", verifyToken, deleteVideo);
+// Get a video
+router.get("/find/:id", getVideo);
+// Get a video view
+router.put("/view/:id", addView);
+// Get trend videos
+router.get("/trend", trend);
+// Get random videos
+router.get("/random", random);
+// Get subscribed videos
+router.get("/sub", verifyToken, sub);
+// Get tagged videos
+router.get("/tags", verifyToken, getByTag);
+// Serach videos by title
+router.get("/search", verifyToken, search);
 
 export default router;
